47cd45d31162b78eba7fc37913499028b289523a
Insu Song <phyuzion@gmail.com>
2025-02-28 10:11:07 +0900

    Revert "Update Mobile"
    This reverts commit 7bf23195ae56fe19a9c5f4ea4ee347c1484c9729.

--- mobile/mobile/android/build.gradle.kts
+++ mobile/mobile/android/build.gradle.kts
@@ -1,15 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // Android Gradle Plugin + Kotlin
-        classpath("com.android.tools.build:gradle:8.8.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
-    }
-}
-
 allprojects {
     repositories {
         google()
