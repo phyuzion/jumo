770748635068422bb1cd5710d8fe818ad4e64a03
Insu Song <phyuzion@gmail.com>
2025-03-08 17:37:29 +0900

    Update

--- mobile/mobile/lib/screens/call_ended_screen.dart
+++ mobile/mobile/lib/screens/call_ended_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:mobile/controllers/call_log_controller.dart';
 import 'package:mobile/controllers/contacts_controller.dart';
 
 class CallEndedScreen extends StatefulWidget {
@@ -15,21 +12,13 @@ class CallEndedScreen extends StatefulWidget {
 class _CallEndedScreen extends State<CallEndedScreen> {
   String? _displayName;
   String? _phones;
-
-  final callLogController = CallLogController();
   // or additional contact info
   // We'll search from contactsController
 
   @override
-  Future<void> initState() async {
+  void initState() {
     super.initState();
     _loadContactName();
-
-    // 로그 갱신
-    final newLogs = await callLogController.refreshCallLogsWithDiff();
-    if (newLogs.isNotEmpty) {
-      log('[PhoneState] new logs => ${newLogs.length}');
-    }
   }
 
   /// 주소록(이미 저장) 에서 widget.incomingNumber 와 일치하는 contact 찾기
