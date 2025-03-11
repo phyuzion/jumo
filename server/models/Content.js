9169ec98e530f82de5ae89f38ceda9759ffaafad
Insu Song <phyuzion@gmail.com>
2025-03-11 12:16:45 +0900

    Revert "Update Content.js"
    This reverts commit 8385e1cdf6fda3e3172bb3884f9f6913dc2ead14.

--- server/models/Content.js
+++ server/models/Content.js
@@ -3,14 +3,12 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
   userId: { type: String },
-  userName : { type: String },
   comment: { type: String }, // 댓글 내용 (여기도 Quill Delta로 저장할 수도 있지만, 일반 텍스트가 일반적)
   createdAt: { type: Date, default: Date.now },
 });
 
 const contentSchema = new mongoose.Schema({
   userId: { type: String },
-  userName: { type: String },
   type: { type: Number, default: 0 }, // 게시판 카테고리 등
   title: { type: String, default: '' },
   content: { type: mongoose.Schema.Types.Mixed, default: {} },
