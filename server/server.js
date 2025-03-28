a5a6c2b4eed4846384c73b02e694a53d98fa6750
Insu Song <phyuzion@gmail.com>
2025-03-28 22:07:03 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,8 @@ async function startServer() {
 
   const app = express();
   app.use(cors());
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
   // 1) graphqlUploadExpress
   app.use(graphqlUploadExpress());
