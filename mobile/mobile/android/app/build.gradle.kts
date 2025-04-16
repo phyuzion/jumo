ca51d120420fa4ae9bdf23a363976fde3468c93d
Insu Song <phyuzion@gmail.com>
2025-04-16 21:59:14 +0900

    Revert "Update build.gradle.kts"
    This reverts commit ce5ede141306010032a662d1c492848aebc19039.

--- mobile/mobile/android/app/build.gradle.kts
+++ mobile/mobile/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     defaultConfig {
         applicationId = "com.jumo.mobile"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
