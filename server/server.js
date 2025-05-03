66270da90d6c9707b5d25da4e1cfda5e4c4702b4
Insu Song <phyuzion@gmail.com>
2025-05-04 00:11:14 +0900

    release 0.1.8

--- server/server.js
+++ server/server.js
@@ -30,8 +30,8 @@ async function startServer() {
 
   const app = express();
   app.use(cors());
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+  app.use(express.json({ limit: '100mb' }));
+  app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
   // 1) graphqlUploadExpress
   app.use(graphqlUploadExpress());
